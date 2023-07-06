faa945f83adbc606abdb2580bec06799928e897f
Neemakr <neema3041999@gmail.com>
2023-07-06 17:43:09 +0530

    Logical opertors code added

--- data_types_demo.js
+++ data_types_demo.js
@@ -1,2 +1,22 @@
 var a=10;
 console.log("a=", a);
+let a = 10;
+var b=30;
+function f() {
+    if (true) {
+        var b = 9
+ 
+        // It prints 9
+        console.log(b);
+    }
+ 
+    // It gives error as it
+    // defined in if block
+    console.log(b);
+}
+f()
+ 
+// It prints 10
+console.log(a)
+console.log(b);
+
